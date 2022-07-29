634a7f688f479b41e5e07197562e5b3df5470656
yangyang guo <94036888+IceMiaoMiao@users.noreply.github.com>
2022-07-29 20:27:39 +0800

    new app

--- src/app.rs
+++ src/app.rs
@@ -55,7 +55,7 @@ impl eframe::App for TemplateApp {
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
+                ui.menu_button("Click here to quit", |ui| {
                     if ui.button("Quit").clicked() {
                         frame.quit();
                     }
@@ -64,25 +64,35 @@ impl eframe::App for TemplateApp {
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("Here is Left Side Panel");
 
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
+                ui.label("Please input your name here: ");
                 ui.text_edit_singleline(label);
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
+            ui.horizontal(|ui| {
+                ui.label("Please input your age here: ");
+                ui.text_edit_singleline(label);
+            });
+
+            ui.add(egui::Slider::new(value, 0.0..=10.0).text("Flow as your interest"));
+            if ui.button("Click here to plus one").clicked() {
                 *value += 1.0;
+            }
+
+            if ui.button("Click here to minus one").clicked() {
+                *value -= 1.0;
             }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
                     ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
+                    ui.label("Powered by ");
                     ui.hyperlink_to("egui", "https://github.com/emilk/egui");
                     ui.label(" and ");
                     ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
+                    ui.label(" Revise by IceMiao ");
                 });
             });
         });
@@ -90,11 +100,11 @@ impl eframe::App for TemplateApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
+            ui.heading("This is eframe template. If you desire to know more:");
             ui.hyperlink("https://github.com/emilk/eframe_template");
             ui.add(egui::github_link_file!(
                 "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
+                "Click here to get Source code."
             ));
             egui::warn_if_debug_build(ui);
         });
